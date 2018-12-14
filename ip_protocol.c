70b51532a70a963e643d75bc04b7e073904cce37
davi <daviferreirasantog@gmail.com>
2018-12-13 22:40:22 -0300

    checksum feito no odranostyle

--- ip_protocol.c
+++ ip_protocol.c
@@ -1,3 +1,17 @@
 #include "ip_protocol.h"
 
 
+int checksum(u_short *buf, int count){
+	//lembrete dos mininino
+	//no buf passa o header_ip apenas, após zerar o campo checksum
+	//no campo count passa o ip_ihl multiplicada por 2
+	while (count--){
+		sum += *buf++;
+		if(sum & 0xFFFF0000){
+			//teve carry
+			sum &= 0xFFFF;
+			sum++;
+		}
+	}
+	return ~(sum & 0xFFFF);
+}
